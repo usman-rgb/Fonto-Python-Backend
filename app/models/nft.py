7a50832fe0033cd6c720efdd444deaa611fa073c
Muhammad Usman <manoousman469@gmail.com>
2025-05-10 11:12:23 +0500

    Update nft.py

--- app/models/nft.py
+++ app/models/nft.py
@@ -1,20 +1,3 @@
-
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from app.database import Base
-
-class NFT(Base):
-    __tablename__ = "nfts"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    description = Column(String)
-    ipfs_hash = Column(String)
-    xrpl_token_id = Column(String, nullable=True)
-    collection_id = Column(Integer, ForeignKey("collections.id"))
-    owner_id = Column(Integer, ForeignKey("users.id"))
-    owner = relationship("User", back_populates="nfts")
-    collection = relationship("Collection", back_populates="nfts")
-
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from app.database import Base
