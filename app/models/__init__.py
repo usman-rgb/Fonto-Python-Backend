1553ed9c1bf431e699fff54beba7c8daff3b71ac
Muhammad Usman <manoousman469@gmail.com>
2025-05-10 11:09:20 +0500

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from .user import User
 from .nft import NFT
 from .collection import Collection
@@ -8,15 +7,4 @@ from .blog import Blog
 from .help import HelpQuestion
 from .notification import Notification
 from .activity import Activity
-=======
-from .user import User
-from .nft import NFT
-from .collection import Collection
-from .bid import Bid
-from .wallet import Wallet
-from .blog import Blog
-from .help import HelpQuestion
-from .notification import Notification
-from .activity import Activity
->>>>>>> 4a87b24658b42a81de51559557417a96c9a3e5f7
 from .newsletter import Newsletter
