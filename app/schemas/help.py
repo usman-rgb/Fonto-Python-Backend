538e96f3e65e44b16b06e916903d916222344374
Muhammad Usman <manoousman469@gmail.com>
2025-05-10 10:43:27 +0500

    Update help.py

--- app/schemas/help.py
+++ app/schemas/help.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from pydantic import BaseModel
 
 class HelpQuestion(BaseModel):
@@ -7,7 +6,7 @@ class HelpQuestion(BaseModel):
     answer: str
 
     class Config:
-=======
+
 from pydantic import BaseModel
 
 class HelpQuestion(BaseModel):
@@ -16,5 +15,5 @@ class HelpQuestion(BaseModel):
     answer: str
 
     class Config:
->>>>>>> 4a87b24658b42a81de51559557417a96c9a3e5f7
+
         from_attributes = True  # Updated from orm_mode=True
