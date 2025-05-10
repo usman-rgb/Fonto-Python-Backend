9a6846ac1d6c1364af58df64fb6ac9c02cd1f9f9
Muhammad Usman <manoousman469@gmail.com>
2025-05-10 11:15:27 +0500

    Update test_db.py

--- test_db.py
+++ test_db.py
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 from sqlalchemy import create_engine
 from app.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 connection = engine.connect()
 print("Connection successful!")
-=======
-from sqlalchemy import create_engine
-from app.config import DATABASE_URL
-
-engine = create_engine(DATABASE_URL)
-connection = engine.connect()
-print("Connection successful!")
->>>>>>> 4a87b24658b42a81de51559557417a96c9a3e5f7
 connection.close()
