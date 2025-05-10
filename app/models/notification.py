8b06e7682337def3bfbcb10a64d85ed64c46e1b0
Muhammad Usman <manoousman469@gmail.com>
2025-05-10 11:12:42 +0500

    Update notification.py

--- app/models/notification.py
+++ app/models/notification.py
@@ -9,14 +9,4 @@ class Notification(Base):
     message = Column(String)
     user_id = Column(Integer, ForeignKey("users.id"))
 
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from app.database import Base
-
-class Notification(Base):
-    __tablename__ = "notifications"
-    id = Column(Integer, primary_key=True, index=True)
-    message = Column(String)
-    user_id = Column(Integer, ForeignKey("users.id"))
-
     user = relationship("User", back_populates="notifications")
