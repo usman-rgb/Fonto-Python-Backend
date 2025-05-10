bbf6999cc633ed745a7376584e6dcf370506b0c6
Muhammad Usman <manoousman469@gmail.com>
2025-05-10 11:09:51 +0500

    Update activity.py

--- app/models/activity.py
+++ app/models/activity.py
@@ -6,12 +6,4 @@ class Activity(Base):
     id = Column(Integer, primary_key=True, index=True)
     action = Column(String)
 
-from sqlalchemy import Column, Integer, String
-from app.database import Base
-
-class Activity(Base):
-    __tablename__ = "activities"
-    id = Column(Integer, primary_key=True, index=True)
-    action = Column(String)
-
     timestamp = Column(String)
