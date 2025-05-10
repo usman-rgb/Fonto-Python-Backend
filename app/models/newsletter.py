00565ac66e33ed2a6e6a7187827a4fb97f1970e3
Muhammad Usman <manoousman469@gmail.com>
2025-05-10 11:12:02 +0500

    Update newsletter.py

--- app/models/newsletter.py
+++ app/models/newsletter.py
@@ -5,11 +5,4 @@ class Newsletter(Base):
     __tablename__ = "newsletters"
     id = Column(Integer, primary_key=True, index=True)
 
-from sqlalchemy import Column, Integer, String
-from app.database import Base
-
-class Newsletter(Base):
-    __tablename__ = "newsletters"
-    id = Column(Integer, primary_key=True, index=True)
-
     email = Column(String, unique=True, index=True)
