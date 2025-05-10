937de50e05d3052e95fe1ae19fddf287ce592f01
Muhammad Usman <manoousman469@gmail.com>
2025-05-10 10:41:44 +0500

    Update activity.py

--- app/schemas/activity.py
+++ app/schemas/activity.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from pydantic import BaseModel
 
 class Activity(BaseModel):
@@ -7,7 +6,7 @@ class Activity(BaseModel):
     timestamp: str
 
     class Config:
-=======
+        
 from pydantic import BaseModel
 
 class Activity(BaseModel):
@@ -16,5 +15,5 @@ class Activity(BaseModel):
     timestamp: str
 
     class Config:
->>>>>>> 4a87b24658b42a81de51559557417a96c9a3e5f7
+
         from_attributes = True  # Updated from orm_mode=True
