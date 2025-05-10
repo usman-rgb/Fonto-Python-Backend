a2153e8b6a668e544bd7a5d323da67d583e0fa94
Muhammad Usman <manoousman469@gmail.com>
2025-05-10 11:11:42 +0500

    Update help.py

--- app/models/help.py
+++ app/models/help.py
@@ -6,12 +6,4 @@ class HelpQuestion(Base):
     id = Column(Integer, primary_key=True, index=True)
     question = Column(String)
 
-from sqlalchemy import Column, Integer, String
-from app.database import Base
-
-class HelpQuestion(Base):
-    __tablename__ = "help_questions"
-    id = Column(Integer, primary_key=True, index=True)
-    question = Column(String)
-
     answer = Column(String)
