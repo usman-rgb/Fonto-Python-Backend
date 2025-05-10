33006fa08ccea20d552b3e637cc138be28e9d77d
Muhammad Usman <manoousman469@gmail.com>
2025-05-10 10:25:50 +0500

    Update base.py

--- app/models/base.py
+++ app/models/base.py
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 from sqlalchemy.ext.declarative import declarative_base
 
-=======
+
 from sqlalchemy.ext.declarative import declarative_base
 
->>>>>>> 4a87b24658b42a81de51559557417a96c9a3e5f7
+
 Base = declarative_base()
