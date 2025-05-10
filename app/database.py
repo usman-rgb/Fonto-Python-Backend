951cdbcd9f38b6025904fc252411e0c4a8ea63f2
Muhammad Usman <manoousman469@gmail.com>
2025-05-10 10:52:21 +0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -22,28 +21,4 @@ def get_db():
         yield db
     finally:
         db.close()
-=======
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-# Environment variable se database URL fetch karo
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
-if not SQLALCHEMY_DATABASE_URL:
-    raise ValueError("SQLALCHEMY_DATABASE_URL environment variable is not set")
-
-# PostgreSQL ke liye async=False set karo
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"sslmode": "require"})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-# Dependency to get DB session
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
->>>>>>> 4a87b24658b42a81de51559557417a96c9a3e5f7
