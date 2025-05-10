7944e7bd1336aed26744ce05d07557a40c69eccd
Muhammad Usman <manoousman469@gmail.com>
2025-05-10 11:10:39 +0500

    Update bid.py

--- app/models/bid.py
+++ app/models/bid.py
@@ -10,16 +10,4 @@ class Bid(Base):
     amount = Column(Float)
     nft = relationship("NFT", back_populates="bids")
 
-from sqlalchemy import Column, Integer, Float, ForeignKey
-from sqlalchemy.orm import relationship
-from app.database import Base
-
-class Bid(Base):
-    __tablename__ = "bids"
-    id = Column(Integer, primary_key=True, index=True)
-    nft_id = Column(Integer, ForeignKey("nfts.id"))
-    bidder_id = Column(Integer, ForeignKey("users.id"))
-    amount = Column(Float)
-    nft = relationship("NFT", back_populates="bids")
-
     bidder = relationship("User", back_populates="bids")
