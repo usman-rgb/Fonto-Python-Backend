6e6969c7408ae37132e733cfa45fe5dd1ceaae36
Muhammad Usman <manoousman469@gmail.com>
2025-05-10 10:32:09 +0500

    Update blog.py

--- app/models/blog.py
+++ app/models/blog.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from sqlalchemy import Column, Integer, String
 from app.database import Base
 
@@ -6,7 +5,7 @@ class Blog(Base):
     __tablename__ = "blogs"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
-=======
+
 from sqlalchemy import Column, Integer, String
 from app.database import Base
 
@@ -14,5 +13,5 @@ class Blog(Base):
     __tablename__ = "blogs"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
->>>>>>> 4a87b24658b42a81de51559557417a96c9a3e5f7
+
     content = Column(String)
