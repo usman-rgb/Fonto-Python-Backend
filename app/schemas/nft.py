6ffa8651362900997c8adc4c01b10b5f5bbd820a
Muhammad Usman <manoousman469@gmail.com>
2025-05-10 10:58:25 +0500

    Update nft.py

--- app/schemas/nft.py
+++ app/schemas/nft.py
@@ -16,22 +16,4 @@ class NFT(BaseModel):
 
     class Config:
 
-from pydantic import BaseModel
-
-class NFTCreate(BaseModel):
-    name: str
-    description: str
-    collection_id: int
-
-class NFT(BaseModel):
-    id: int
-    name: str
-    description: str
-    ipfs_hash: str
-    xrpl_token_id: str = None
-    collection_id: int
-    owner_id: int
-
-    class Config:
-
         from_attributes = True  # Updated from orm_mode=True
