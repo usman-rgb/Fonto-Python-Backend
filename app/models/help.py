496ae6623933e409980f260fe927bc5d5228000f
Muhammad Usman <manoousman469@gmail.com>
2025-05-10 10:33:03 +0500

    Update help.py

--- app/models/help.py
+++ app/models/help.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from sqlalchemy import Column, Integer, String
 from app.database import Base
 
@@ -6,7 +5,7 @@ class HelpQuestion(Base):
     __tablename__ = "help_questions"
     id = Column(Integer, primary_key=True, index=True)
     question = Column(String)
-=======
+
 from sqlalchemy import Column, Integer, String
 from app.database import Base
 
@@ -14,5 +13,5 @@ class HelpQuestion(Base):
     __tablename__ = "help_questions"
     id = Column(Integer, primary_key=True, index=True)
     question = Column(String)
->>>>>>> 4a87b24658b42a81de51559557417a96c9a3e5f7
+
     answer = Column(String)
