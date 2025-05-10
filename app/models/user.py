cedb44e0382a4003297c63534f2d5334277244d8
Muhammad Usman <manoousman469@gmail.com>
2025-05-10 11:13:02 +0500

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,18 +1,3 @@
-
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from app.database import Base
-
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    username = Column(String, nullable=True)
-    hashed_password = Column(String)
-    nfts = relationship("NFT", back_populates="owner")
-    bids = relationship("Bid", back_populates="bidder")
-    wallet = relationship("Wallet", back_populates="user", uselist=False)
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from app.database import Base
