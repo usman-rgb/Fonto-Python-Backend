4173d2521c4ac980ae3793e399293bf89c3d0f4d
Muhammad Usman <manoousman469@gmail.com>
2025-05-10 11:10:56 +0500

    Update blog.py

--- app/models/blog.py
+++ app/models/blog.py
@@ -6,12 +6,4 @@ class Blog(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
 
-from sqlalchemy import Column, Integer, String
-from app.database import Base
-
-class Blog(Base):
-    __tablename__ = "blogs"
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-
     content = Column(String)
