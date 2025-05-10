93d13e031356e0e6bafb635ef5cfd09713d34fce
Muhammad Usman <manoousman469@gmail.com>
2025-05-10 11:11:22 +0500

    Update collection.py

--- app/models/collection.py
+++ app/models/collection.py
@@ -8,14 +8,4 @@ class Collection(Base):
     name = Column(String)
     description = Column(String)
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from app.database import Base
-
-class Collection(Base):
-    __tablename__ = "collections"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    description = Column(String)
-
     nfts = relationship("NFT", back_populates="collection")
