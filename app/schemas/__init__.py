5f4fc143d0277fca0c07d3691d790c5afdbb4869
Muhammad Usman <manoousman469@gmail.com>
2025-05-10 10:35:53 +0500

    Update __init__.py

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from .user import User, UserCreate, Token, PasswordResetRequest
 from .nft import NFT, NFTCreate
 from .collection import Collection, CollectionCreate
@@ -8,7 +7,6 @@ from .blog import Blog
 from .help import HelpQuestion
 from .notification import Notification
 from .activity import Activity
-=======
 from .user import User, UserCreate, Token, PasswordResetRequest
 from .nft import NFT, NFTCreate
 from .collection import Collection, CollectionCreate
@@ -18,5 +16,4 @@ from .blog import Blog
 from .help import HelpQuestion
 from .notification import Notification
 from .activity import Activity
->>>>>>> 4a87b24658b42a81de51559557417a96c9a3e5f7
 from .newsletter import Newsletter
